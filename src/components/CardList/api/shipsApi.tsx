9eef379ce902adfa56f8840ba537080274005152
Andrey Morogin <andreandreandre10071998@yandex.ru>
2023-10-31 12:34:45 +0300

    feat: добавить подержку graphQL

--- src/components/CardList/api/shipsApi.tsx
+++ src/components/CardList/api/shipsApi.tsx
@@ -1,17 +1,34 @@
 import axios from "axios";
+import { AxiosError } from "axios";
 
-import { API_URL } from "../constants/constants";
+import { API_URL, GET_QUERY_SHIPS, IShip } from "../constants/constants";
 
-const api = axios.create({
-  baseURL: API_URL,
-});
+interface Result<T> {
+  hasError: boolean;
+  errorMessage: string;
+  data: T | null
+}
 
-async function getPosts(start, length) {
-    const { data } = await api.get(`/posts?_start=${start}&_limit=${length}`);
+async function getShips() {
+  const result: Result<IShip> = {
+    hasError: false,
+    errorMessage: "",
+    data: null,
+  }
 
-    return data;
+  try {
+    const serverData = await axios.post(API_URL, { GET_QUERY_SHIPS });
+    const dataShips = serverData.data.vehicles;
+    result.data = dataShips;
+  } catch(error) {
+    const err = error as AxiosError;
+    result.hasError = true;
+    result.errorMessage = err.message;
+  }
+
+  return result;
 }
 
 export {
-  getPosts,
+  getShips,
 };
--- src/components/CardList/constants/constants.ts
+++ src/components/CardList/constants/constants.ts
@@ -1,3 +1,49 @@
 const API_URL = 'https://vortex.korabli.su/api/graphql/glossary';
 
-export { API_URL };
+const GET_QUERY_SHIPS = `
+query vehicles {
+  vehicles {
+    id
+    title
+    description
+    level
+    nationName
+    typeName
+    icons {
+        medium
+    }
+  }
+}`;
+
+interface IShip {
+  description: string;
+  icons: {
+    large: string;
+    medium: string;
+  };
+  level: number;
+  nation: {
+    color: string;
+    icons: {
+      large: string;
+      medium: string;
+      small: string;
+    };
+    name: string;
+    title: string;
+  };
+  title: string;
+  type: {
+    icons: {
+      default: string;
+    };
+    name: string;
+    title: string;
+  };
+}
+
+export { 
+  API_URL,
+  GET_QUERY_SHIPS,
+  IShip
+};
