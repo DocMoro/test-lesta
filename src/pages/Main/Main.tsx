5101f8d21afb481931bc135a6476b17b4a33b4bd
Andrey Morogin <andreandreandre10071998@yandex.ru>
2023-11-14 20:41:20 +0300

    delete: вырезать VideoFrame

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,12 +1,10 @@
 import './Main.scss';
 
 import CardList from '../../components/CardList/CardList';
-import VideoFrame from '../../components/VideoFrame/VideoFrame';
 
 export default function Main() {
   return (
     <main className="page">
-      <VideoFrame />
       <CardList />
     </main>
   );
